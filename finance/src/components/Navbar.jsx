4835ad26c19011ac3f0170958e6459c28004fc19
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-21 16:15:17 +0200

    installed react-icons. in import must add react-icons/fa.

--- finance/src/components/Navbar.jsx
+++ finance/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import logo 
+import logo from "./images/logo.png"
+import {FaBars, FaTimes} from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className='header'>
       <nav className="navbar">
         <a href="/" className="logo">
+           <img src={logo} alt="logo" />
         </a>
+            <div className="hamburger">
+
+            </div>
       </nav>
     </div>
   )

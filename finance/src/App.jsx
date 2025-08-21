952925a540cc4dea0800929cf3a26f1b44614dfd
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-21 20:27:50 +0200

    added user/s opinions.

--- finance/src/App.jsx
+++ finance/src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
+import Testimonials from "./components/Testimonials"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Navbar/>
             <Home/>
             <About/>
+            <Testimonials/>
            </div>
     </>
   )
--- finance/src/components/Testimonials.jsx
+++ finance/src/components/Testimonials.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import "./Testimonials.css"
+import user1 from './images/user1.jpeg'
+import user2 from './images/user2.jpeg'
+import user3 from './images/user3.jpeg'
 const Testimonials = () => {
   return (
-    <div>
-      
+    <div className='testimonials'>
+        <div className="container">
+             <h2>Testimonials</h2>
+             <span className="line"></span>
+             <div className="content">
+              <div className="card">
+                <img src={user1} alt="user1" />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem amet fugit quisquam molestiae repudiandae provident pariatur accusantium, expedita modi voluptatum officiis maxime? Quibusdam doloremque rerum placeat impedit quasi adipisci asperiores!</p>
+                <p><span>Name </span></p>
+                <p>Position</p>
+              </div>
+              <div className="card">
+                <img src={user2} alt="user2" />
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem amet fugit quisquam molestiae repudiandae provident pariatur accusantium, expedita modi voluptatum officiis maxime? Quibusdam doloremque rerum placeat impedit quasi adipisci asperiores!</p>
+                <p><span>Name </span></p>
+                <p>Position</p>
+              </div>
+              <div className="card">
+                <img src={user3} alt="user3" />
+           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem amet fugit quisquam molestiae repudiandae provident pariatur accusantium, expedita modi voluptatum officiis maxime? Quibusdam doloremque rerum placeat impedit quasi adipisci asperiores!</p>
+                <p><span>Name </span></p>
+                <p>Position</p>
+              </div>
+             </div>
+        </div>
     </div>
   )
 }

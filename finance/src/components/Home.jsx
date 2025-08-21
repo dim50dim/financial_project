110f128d2bf2c5a1b2b24d6560fec26210b1f52a
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-21 18:06:38 +0200

    also created  Home.css

--- finance/src/components/Home.jsx
+++ finance/src/components/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='hero'>
+        <div className="content">
+            <p>Call us</p>
+            <p>455-3-434-3</p>
+            <p>Money</p>
+            <p>Does't come with instructions</p>
+            <button href="/" className='button'>Free Consultation</button>
+        </div>
       
     </div>
   )

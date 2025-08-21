85a2d2fda4da3e40dac227915f9fd8866fb8eed0
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-21 18:45:53 +0200

    styled About.css

--- finance/src/components/About.jsx
+++ finance/src/components/About.jsx
@@ -9,10 +9,15 @@ const About = () => {
          <div className="about">
             <div className="container">
                  <img src={john}/>
-                 <d className="col-2">
+                 <div className='col-2'>
                     <h2>About</h2>
                     <span className="line"></span>
-                 </d>
+                       <p>Intense is an International Financial Planning company with offices i n multiple jurisdictions over the world. Working with Intense gives me the ability to advise internat ional expatriates living in the middle east from where I live in USA.</p>
+                    <p>I am John Doe, a senior advisor for an independently owned financial planning company called Intense.</p>
+                     <button className='button'>
+                          Explore more
+                     </button>
+                 </div>
             </div>
          </div>
     
